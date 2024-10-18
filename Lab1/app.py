0e97f2af32d37c4e6d0b10d7609b78359a227669
Oillim <nguyenminh29803@gmail.com>
2024-10-18 09:10:35 +0700

    fixed function 1 & add more testcases

--- Lab1/app.py
+++ Lab1/app.py
@@ -14,11 +14,12 @@ if __name__ == '__main__':
     if choice == '1':
         subject = input('Enter email subject: ')
         message = input('Enter email message: ')
-        text = pd.DataFrame(columns=['Text'], data=[subject + " " + message])
+        text = [subject + " " + message]
         if pipeline.predict(text)[0] == 1:
-            print('\nThis is Spam email.')
+            print('\nThis is a Spam email.')
         else:
-            print('\nThis is not Spam email.')
+            print('\nThis is not a Spam email.')
+
     elif choice == '2':
         #Load data
         file_path = input('Enter file path: ')
@@ -33,24 +34,22 @@ if __name__ == '__main__':
         df.fillna('', inplace=True)
         df['Text'] = df['Subject'] + " " + df['Message']
         df.drop(['Subject', 'Message'], axis=1, inplace=True)
-
         y = df['Spam/Ham'].map({'spam': 1, 'ham': 0})
 
         #Predict
         y_pred = pd.Series(pipeline.predict(df.Text))
-        # for i in range(len(pred)):
-        #     if pred[i] == 1:
-        #         print(f'{df.iloc[i, 0]} is Spam email.')
-        #     else:
-        #         print(f'{df.iloc[i, 0]} is not Spam email.')
-        
+
         #Save to output.csv
         df['Spam/Ham Predict'] = y_pred.map({1: 'spam', 0: 'ham'})
         df = df[['Message ID', 'Spam/Ham', 'Spam/Ham Predict']]
+        df.to_csv('output.csv', index=False)
+
+        #Print metrics
         print('Accuracy:', accuracy_score(y, y_pred))
         print(classification_report(y, y_pred, digits=4))
-        df.to_csv('output.csv', index=False)
         print('Output saved to output.csv')
+
+
     elif choice == '3':
         print('Exiting...')
     else:
