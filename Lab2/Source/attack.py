5eecede50819aef0b45f10fd8e6cdbe13adbcca1
hoaithi <phunghoaithi137@gmail.com>
2024-10-31 16:00:22 +0700

    add: attack

--- Lab2/Source/attack.py
+++ Lab2/Source/attack.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import data_processing as dp
 import feature_extraction as fe
+import matplotlib.pyplot as plt
 
-sampling = 3
+# Tải dữ liệu
 (x_train, y_train), (x_test, y_test) = dp.load_data_keras("../Data")
-(x_test, y_test) = fe.ResnetPreprocess(x_test=x_test, y_test=y_test, sampling=sampling, test=True)
+(x_test, y_test) = fe.HogPreprocess(x_val=x_test, y_val=y_test)
+print("Shape của tập kiểm tra:", x_test.shape)
 
-model = tf.keras.models.load_model('./model/federate_learning_model.keras')
+# Tải mô hình
+model = tf.keras.models.load_model('./model/hog_model.keras')
 
-def create_adversarial_example(model, image, label, epsilon=0.03, alpha=0.01, num_steps=10):
+def create_adversarial_example(model, image, label, epsilon=0.003, alpha=0.01, num_steps=10):
     image = tf.convert_to_tensor(image)
     label = tf.convert_to_tensor(label)
 
@@ -27,32 +29,37 @@ def create_adversarial_example(model, image, label, epsilon=0.03, alpha=0.01, nu
         image = image + alpha * tf.sign(gradient)
 
         image = tf.clip_by_value(image, original_image - epsilon, original_image + epsilon)
-        image = tf.clip_by_value(image, 0, 1)  
+        image = tf.clip_by_value(image, 0, 1)
 
     return image
 
-image_index = 0  
-image = x_test[image_index:image_index+1]
-label = y_test[image_index:image_index+1]
+epsilon_values = [0.0005, 0.001, 0.002, 0.005, 0.01, 0.02]  # Các giá trị epsilon để thử nghiệm
+adversarial_accuracies = []
 
-adversarial_image = create_adversarial_example(model, image, label)
+total_images = len(x_test)
+
+for epsilon in epsilon_values:
+    correct_adversarial_predictions = 0
+
+    for i in range(50):  
+        image = x_test[i:i+1]
+        label = y_test[i]
+
+        adversarial_image = create_adversarial_example(model, image, label, epsilon=epsilon)
+
+        adversarial_prediction = np.argmax(model.predict(adversarial_image))
+
+        if adversarial_prediction == label:
+            correct_adversarial_predictions += 1
+
+    adversarial_accuracy = correct_adversarial_predictions / 100  
+    adversarial_accuracies.append(adversarial_accuracy)
 
 plt.figure(figsize=(10, 5))
-
-plt.subplot(1, 2, 1)
-plt.title("Original Image")
-plt.imshow(image[0])
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-plt.title("Adversarial Image")
-plt.imshow(adversarial_image[0].numpy())
-plt.axis('off')
-
+plt.plot(epsilon_values, adversarial_accuracies, marker='o')
+plt.title('Tỷ lệ chính xác theo epsilon')
+plt.xlabel('Epsilon')
+plt.ylabel('Tỷ lệ ảnh dự đoán chính xác sau khi tạo đối kháng')
+plt.xticks(epsilon_values)  
+plt.grid()
 plt.show()
-
-original_prediction = np.argmax(model.predict(image))
-adversarial_prediction = np.argmax(model.predict(adversarial_image))
-
-print("Dự đoán ban đầu:", original_prediction)
-print("Dự đoán với ví dụ đối kháng:", adversarial_prediction)
